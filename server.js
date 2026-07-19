5313c2f3790a5eb663dd2d461a5dcb479933d42e
mahipalsinghsekhawat26-max <mahipalsinghsekhawat26@gmail.com>
2026-07-19 18:11:24 +0530

    Use environment variable for server port

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const marketData = require('./data.json');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
